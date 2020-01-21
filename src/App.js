7cb067a5b38b6b057d3cedae090627267de638c4
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2020-01-21 13:43:41 -0300

    listing devs

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import './Sidebar.css'
 import './Main.css'
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
   const [github_username, setGithubUsername] = useState('');
   const [techs, setTechs] = useState('');
-
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
 
@@ -30,6 +31,16 @@ function App() {
     )
   },[]);
 
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
   async function handleAddDev(e) {
     e.preventDefault();
 
@@ -42,6 +53,10 @@ function App() {
 
     setGithubUsername('');
     setTechs('');
+
+    console.log(response.data);
+
+    setDevs([...devs, response.data]);
   }
 
   return (
@@ -103,53 +118,19 @@ function App() {
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/19976090?s=460&v=4" alt="Hikaro Cordeiro"/>
-              <div className="user-info">
-                <strong>Hikaro Cordeiro</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Bio text goes here</p>
-            <a href="https://github.com/hikarocordeiro">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/19976090?s=460&v=4" alt="Hikaro Cordeiro"/>
-              <div className="user-info">
-                <strong>Hikaro Cordeiro</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Bio text goes here</p>
-            <a href="https://github.com/hikarocordeiro">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/19976090?s=460&v=4" alt="Hikaro Cordeiro"/>
-              <div className="user-info">
-                <strong>Hikaro Cordeiro</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Bio text goes here</p>
-            <a href="https://github.com/hikarocordeiro">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/19976090?s=460&v=4" alt="Hikaro Cordeiro"/>
-              <div className="user-info">
-                <strong>Hikaro Cordeiro</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Bio text goes here</p>
-            <a href="https://github.com/hikarocordeiro">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
+              <header>
+                <img src={dev.avatar_url} alt={dev.name} />
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${dev.github_username}`} >Acessar perfil no Github</a>
+            </li>
+          ))}
         </ul>
       </main>
     </div>
