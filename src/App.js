1738ac4e85153b17b25f4cecb04d9a7abc37e14d
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2020-01-21 11:26:11 -0300

    sidebar html

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import React from 'react';
 
+import './global.css'
+import './App.css'
+
 function App() {
   return (
-    <h1>Structure</h1>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+          
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
